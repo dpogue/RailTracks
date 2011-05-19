501962060caa614425f4b3b5bf2f403fb407e0cf
Darryl Pogue <dvpdiner2@gmail.com>
2011-05-18 23:03:24 -0700

    Stop button and sometimes pause functionality.

--- public/javascripts/rtPlayer.js
+++ public/javascripts/rtPlayer.js
@@ -11,9 +11,17 @@ var rtPlayer = function(player_id)
 
     this.audio = root.find('audio');
 
+    this.btnStop = root.find('#btnstop');
+    this.btnMain = root.find('#btnmain');
+
     this.audio.bind('ended', function() {
         self.title.fadeOut('slow');
         self.subtitle.fadeOut('slow');
+        self.btnStop.removeClass('enabled');
+        self.btnStop.unbind('click');
+        self.btnMain.removeClass('enabled');
+        self.btnMain.html('<img alt="Play" src="/images/player/play.png" border="0">');
+        self.btnMain.unbind('click');
         if (self.cursong) { $(self.cursong).removeClass('current'); self.cursong = null; }
     });
 
@@ -25,13 +33,25 @@ var rtPlayer = function(player_id)
 
         var success = function(data, s, jqxhr) {
             self.audio.attr('src', data.url);
+            self.audio.get(0).load();
             self.audio.get(0).play();
 
             self.cursong = '#song-'+data.id;
             $(self.cursong).addClass('current');
+            self.btnStop.addClass('enabled');
+            self.btnStop.bind('click', function(e) {
+                self.stop();
+                e.preventDefault();
+            });
 
             self.audio.bind('playing', function() {
                 self.spinner.hide();
+                self.btnMain.html('<img alt="Pause" src="/images/player/pause.png" border="0">');
+                self.btnMain.addClass('enabled');
+                self.btnMain.bind('click', function(e) {
+                    self.pause();
+                    e.preventDefault();
+                });
                 self.title.html(data.title).fadeIn('slow');
                 self.subtitle.html(data.artist + ' &nbsp;&mdash;&nbsp; ' + data.album).fadeIn('slow');
             });
@@ -42,6 +62,30 @@ var rtPlayer = function(player_id)
             success: success,
             type: 'GET'
         });
+    };
+
+    this.stop = function() {
+        self.audio.get(0).pause(); /* No stop() for <audio> */
+        self.audio.attr('src', '');
+        self.title.hide();
+        self.subtitle.hide();
+        self.spinner.hide();
+        self.btnStop.removeClass('enabled');
+        self.btnStop.unbind('click');
+        self.btnMain.removeClass('enabled');
+        self.btnMain.html('<img alt="Play" src="/images/player/play.png" border="0">');
+        self.btnMain.unbind('click');
+        if (self.cursong) { $(self.cursong).removeClass('current'); self.cursong = null; }
+    };
+
+    this.pause = function() {
+        if (self.audio.get(0).paused) {
+            self.audio.get(0).play();
+            self.btnMain.html('<img alt="Pause" src="/images/player/pause.png" border="0">');
+        } else {
+            self.audio.get(0).pause();
+            self.btnMain.html('<img alt="Play" src="/images/player/play.png" border="0">');
+        }
     };
 
     this.on_page_load = function() {
