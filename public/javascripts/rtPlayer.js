9d7c9ef78993ca6378eb98c848510ef5f3ba2f6b
Darryl Pogue <dvpdiner2@gmail.com>
2011-05-19 08:56:31 -0700

    Playing/Pausing working correctly.

--- public/javascripts/rtPlayer.js
+++ public/javascripts/rtPlayer.js
@@ -4,26 +4,14 @@ var rtPlayer = function(player_id)
 
     var root = $(player_id);
     this.cursong = null;
+    this.audio = null;
 
     this.title = root.find('h1');
     this.subtitle = root.find('h2');
     this.spinner = root.find('.spinner');
 
-    this.audio = root.find('audio');
-
     this.btnStop = root.find('#btnstop');
     this.btnMain = root.find('#btnmain');
-
-    this.audio.bind('ended', function() {
-        self.title.fadeOut('slow');
-        self.subtitle.fadeOut('slow');
-        self.btnStop.removeClass('enabled');
-        self.btnStop.unbind('click');
-        self.btnMain.removeClass('enabled');
-        self.btnMain.html('<img alt="Play" src="/images/player/play.png" border="0">');
-        self.btnMain.unbind('click');
-        if (self.cursong) { $(self.cursong).removeClass('current'); self.cursong = null; }
-    });
 
     this.play_file = function(id) {
         if (self.cursong) { $(self.cursong).removeClass('current'); }
@@ -32,26 +20,20 @@ var rtPlayer = function(player_id)
         self.spinner.show();
 
         var success = function(data, s, jqxhr) {
-            self.audio.attr('src', data.url);
-            self.audio.get(0).load();
-            self.audio.get(0).play();
+            self.audio = new Audio(data.url);
+            self.audio.addEventListener('ended', self.stop);
+            self.audio.play();
 
             self.cursong = '#song-'+data.id;
             $(self.cursong).addClass('current');
             self.btnStop.addClass('enabled');
-            self.btnStop.bind('click', function(e) {
-                self.stop();
-                e.preventDefault();
-            });
+            self.btnStop.bind('click', self.stop);
 
-            self.audio.bind('playing', function() {
+            self.audio.addEventListener('playing', function() {
                 self.spinner.hide();
                 self.btnMain.html('<img alt="Pause" src="/images/player/pause.png" border="0">');
                 self.btnMain.addClass('enabled');
-                self.btnMain.bind('click', function(e) {
-                    self.pause();
-                    e.preventDefault();
-                });
+                self.btnMain.bind('click', self.pause);
                 self.title.html(data.title).fadeIn('slow');
                 self.subtitle.html(data.artist + ' &nbsp;&mdash;&nbsp; ' + data.album).fadeIn('slow');
             });
@@ -65,8 +47,8 @@ var rtPlayer = function(player_id)
     };
 
     this.stop = function() {
-        self.audio.get(0).pause(); /* No stop() for <audio> */
-        self.audio.attr('src', '');
+        if (self.audio) { self.audio.pause(); }
+        self.audio = null;
         self.title.hide();
         self.subtitle.hide();
         self.spinner.hide();
@@ -76,16 +58,18 @@ var rtPlayer = function(player_id)
         self.btnMain.html('<img alt="Play" src="/images/player/play.png" border="0">');
         self.btnMain.unbind('click');
         if (self.cursong) { $(self.cursong).removeClass('current'); self.cursong = null; }
+        return false;
     };
 
     this.pause = function() {
-        if (self.audio.get(0).paused) {
-            self.audio.get(0).play();
+        if (self.audio && self.audio.paused) {
+            self.audio.play();
             self.btnMain.html('<img alt="Pause" src="/images/player/pause.png" border="0">');
-        } else {
-            self.audio.get(0).pause();
+        } else if (self.audio) {
+            self.audio.pause();
             self.btnMain.html('<img alt="Play" src="/images/player/play.png" border="0">');
         }
+        return false;
     };
 
     this.on_page_load = function() {
