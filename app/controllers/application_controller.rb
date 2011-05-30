71809a05d144fa894be74be0435c5d170498f0cc
Darryl Pogue <dvpdiner2@gmail.com>
2011-05-30 00:15:31 -0700

    New layout code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  layout :setlayout
+
   protected
   def pjax?
     return true if request.headers['X-PJAX']
+  end
+
+  def setlayout
+    return 'unauth' if devise_controller?
+    return 'application'
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def inline_svg(path)
+    File.open(asset_file_path!(path)).read.gsub(/\r?\n/, '').gsub(/ ( )+/, '').html_safe
+  end
 end
